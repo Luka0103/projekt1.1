0908cd830de09264b020eaa12a598ed322f9294e
Luka Strahlendorf <luka.strahlendorf@hotmail.com>
2022-11-01 17:53:58 +0100

    Labor 4.9

--- projekt1.1/main.c
+++ projekt1.1/main.c
@@ -3,42 +3,39 @@
 
 
 //Constanten
-#define MAX_EINGABE 10
 
-int eingabe[MAX_EINGABE];
-int pruefung;
-int index=0;
-
-int main() {
-    for (int i = 0; i < MAX_EINGABE; i++)
-    {
-        scanf("%i", &pruefung);
-        if (pruefung>0&&pruefung<7) {            //Ausgeführt wenn richtige Eingabe
-            eingabe[i] = pruefung;
-            index++;
-        }
-        else {
-            break;
-        }
-
-    }
-
-    double summe=0;
-    double durchschnitt;
-
-    for (int i = 0; i <= index; i++)
-    {
-        summe += eingabe[i];
-    }
-    if (index != 0) {
-        durchschnitt = summe / index;
+int teilbar(int zahl1, int zahl2) {//1 wenn teilbar ohne rest 0 wenn nicht
+    if (zahl1 % zahl2 == 0) {
+        return 1;
     }
     else {
-        durchschnitt = 0;
+        return 0;
     }
-    
+}
 
-    printf("Durchschnitt (N=%i): %.2f", index, durchschnitt);
-    
+int main() {
+    int n=0,m = 0;
+    int schreibzahl=0;
+    scanf("%i %i", &n, &m);
+
+    for (int i = 1; i <= 200; i++)
+    {
+        schreibzahl = 1;
+        if (teilbar(i, n)) {
+            printf("fizz");
+            schreibzahl = 0;
+        }
+        if(teilbar(i, m)) {
+            printf("buzz");
+            schreibzahl = 0;
+        }
+        if(schreibzahl) {
+            printf("%i", i);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
+
+
