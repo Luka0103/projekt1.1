170f4c6b7d40ae2aa217288bdfc23913d67e32b5
Luka Strahlendorf <luka.strahlendorf@hotmail.com>
2022-11-01 10:57:11 +0100

    Labor 4.4

--- projekt1.1/main.c
+++ projekt1.1/main.c
@@ -7,23 +7,27 @@
 
 char eingabe[MAX_EINGABE];
 char vokale[] = { 'a','e','i','o','u','A','E','I','O','U' };
-int anzahlVokale = 0;
 int main() {
     for (int i = 0; i < MAX_EINGABE; i++)   //Nicht gut besser wäre scanf mit %s aber nimmt moodle nicht
     {
-        scanf("%c", &eingabe[i]);
+        scanf("%c", &eingabe[i]);           //eingabe0 == ersatz eingabe1== leerzeichen
     }
 
-    for (int i = 0; i < MAX_EINGABE; i++)
+    for (int i = 2; i < MAX_EINGABE; i++)   //start bei eingabe2
     {
         for (int j = 0; j < 10; j++)
         {
             if (eingabe[i] == vokale[j]) {
-                anzahlVokale++;
+                eingabe[i] = eingabe[0];
             }
         }
     }
-    printf("%i Vokale", anzahlVokale);
-    
+    for (int i = 2; i < MAX_EINGABE; i++)
+    {
+        if (eingabe[i] != '\x00') {
+            printf("%c", eingabe[i]);
+        }
+        
+    }
     return 0;
 }
