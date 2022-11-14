5daf00efceef73c0e08957dc9bac21064e496365
Luka Strahlendorf <luka.strahlendorf@hotmail.com>
2022-11-14 10:17:40 +0100

    Labor 5.11

--- projekt1.1/main.c
+++ projekt1.1/main.c
@@ -1,41 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-
-//Constanten
-
-int teilbar(int zahl1, int zahl2) {//1 wenn teilbar ohne rest 0 wenn nicht
-    if (zahl1 % zahl2 == 0) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-int main() {
-    int n=0,m = 0;
-    int schreibzahl=0;
-    scanf("%i %i", &n, &m);
-
-    for (int i = 1; i <= 200; i++)
-    {
-        schreibzahl = 1;
-        if (teilbar(i, n)) {
-            printf("fizz");
-            schreibzahl = 0;
-        }
-        if(teilbar(i, m)) {
-            printf("buzz");
-            schreibzahl = 0;
-        }
-        if(schreibzahl) {
-            printf("%i", i);
-        }
-        printf("\n");
-    }
-
+int main(int argc, char* argv[]) {
+    int value = 42;
+    int* pointerValue = &value;
+    printf("%d\n", *pointerValue);
+    *pointerValue = 85;
+    printf("%d\n", *pointerValue);
+    *pointerValue = *pointerValue + 1;
+    printf("%d\n", *pointerValue);
     return 0;
 }
-
-
